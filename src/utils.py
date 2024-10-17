3d3809a41d02764d0d94ea712ea96f350635b65c
takaya-hirano-hayashibeLabo <takaya.hirano.p5@dc.tohoku.ac.jp>
2024-10-17 15:46:21 +0900

    fix: encoding=utf-8

--- src/utils.py
+++ src/utils.py
@@ -41,7 +41,7 @@ def load_yaml(file_path):
     :return: Dictionary containing the YAML file contents.
     """
     import yaml
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r',encoding="utf-8") as file:
         return yaml.safe_load(file)
 
 
