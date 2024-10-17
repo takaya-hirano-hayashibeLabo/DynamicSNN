1a23f5a4f7044c580386d4fbeda2f3aa11071155
takaya-hirano-hayashibeLabo <takaya.hirano.p5@dc.tohoku.ac.jp>
2024-10-17 23:16:24 +0900

    train: ellipse with noise

--- train-trajectory/train_trajectory.py
+++ train-trajectory/train_trajectory.py
@@ -211,6 +211,8 @@ def main():
     save_dict2json(input_nrm_params,resultpath/"input_nrm_params.json")
 
     input_nrm_datas=2*((input_datas-input_min)/(input_max-input_min))[1:].values - 1 #入力データの正規化
+    in_noise=np.random.normal(loc=0,scale=0.1,size=input_nrm_datas.shape)
+    input_nrm_datas+=in_noise
     input_nrm_datas=create_windows(
         torch.Tensor(input_nrm_datas),
         window=base_sequence,
