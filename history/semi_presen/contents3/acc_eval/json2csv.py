84c0aca09c76266143beabe7f04d9219f8701e76
takaya-hirano-hayashibeLabo <takaya.hirano.p5@dc.tohoku.ac.jp>
2024-11-05 19:22:51 +0900

    add: contents3/ json2csv

---
+++ history/semi_presen/contents3/acc_eval/json2csv.py
@@ -0,0 +1,39 @@
+
+import os
+import json
+import pandas as pd
+from pathlib import Path
+PARENT=Path(__file__).parent
+
+import argparse
+
+def json_to_csv(directory, output_path,outname="results.csv"):
+    data = []
+    
+    # JSONファイルを読み込む
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            with open(os.path.join(directory, filename), 'r') as file:
+                json_data = json.load(file)
+                data.append(json_data)
+
+    # DataFrameに変換
+    df = pd.DataFrame(data)
+    # timescaleカラムで昇順に並び替え
+    df = df.sort_values(by='time-scale')
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # CSVファイルとして保存
+    df.to_csv(output_path/outname, index=False)
+
+
+def main():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--jsonpath",default=PARENT/"results")
+    args=parser.parse_args()
+
+    json_to_csv(Path(args.jsonpath), Path(args.jsonpath),outname="results.csv")
+
+if __name__=="__main__":
+    main()
